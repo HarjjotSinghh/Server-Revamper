bd38e31c8ae6c5955d3337e2864bc34806481c11
VideroDesigns <maxayprivate@gmail.com>
2020-12-15 05:51:33 +0530

    [👻] Some more changes

--- code/src/commands/revamp/RevampCommand.js
+++ code/src/commands/revamp/RevampCommand.js
@@ -1,16 +1,8 @@
 require('dotenv').config();
-const BaseCommand = require('../../utils/structures/BaseCommand');
-const { Pool } = require('pg');
-const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'server-revamper',
-  password: process.env['DATABASE_PW']
-});
-pool.on('error', (err, client) => {
-  console.error('Error:\n', err);
-});
-module.exports = class RevampCommand extends BaseCommand {
+import BaseCommand from '../../utils/structures/BaseCommand';
+import mongoose from 'mongoose';
+
+export default class RevampCommand extends BaseCommand {
   constructor() {
     super('revamp', 'revamp', []);
   }
@@ -19,10 +11,10 @@ module.exports = class RevampCommand extends BaseCommand {
     if (args.length > 0 && args[0]  === 'server') {
       var x = message.channel.send("React with an emoji!");
       const filter = (reaction, user) => {
-        return user.id === message.author.id //&& reaction.message.id === x.id
-      }
+        return user.id === message.author.id // && reaction.message.id === x.id
+      };
       message.channel.send("React with an emoji!").then((message) => {
-        message.awaitReactions(filter, {max: 1, time: 120 * 1000, errors: ['time']})
+        message.awaitReactions(filter, {max: 1, time: 120000, errors: ['time']})
         .then(collected => {
           const reaction = collected.first();
           const one = "┃";
@@ -32,7 +24,7 @@ module.exports = class RevampCommand extends BaseCommand {
           const five = "║";
           const six = "╠";
           const seven = "▪";
-          message.channel.send(`You selected the \\${reaction.emoji.name} Emoji!\nNow lets select a type of divider!`);
+          message.channel.send(`You selected the \\${reaction.emoji} Emoji!\nNow lets select a type of divider!`);
           const msg = `**${message.author.tag}** Please choose one of the following dividers for the channel names.\n`/
                       `:one: - ${reaction.emoji.name}${one}#channel-name\n`/
                       ":two: - `${reaction.emoji.name}${two}#channel-name`\n"/
@@ -44,8 +36,8 @@ module.exports = class RevampCommand extends BaseCommand {
                       "Please reply with the number of divider you choose below :point_down_tone3:";
           message.channel.send(`You selected the \\${reaction.emoji.name} Emoji!\nNow lets select a type of divider!`);
         })
-        .catch(() => {
-          console.error;
+        .catch((err) => {
+          // console.error(err);
           message.channel.send(`You didn't react with an emoji in time! Try again!`);
         });
       });
@@ -53,10 +45,10 @@ module.exports = class RevampCommand extends BaseCommand {
     }
     else {
       if (args.length === 0) {
-        return await message.channel.send("no args given, invoke s!help revamp");
+        return await message.channel.send("no args given, invoke **\`s!help revamp\`**");
       }
       else if (args.length > 0) {
-        return await message.channel.send(`"s!revamp ${args.join(" ")}" is not a valid command`);
+        return await message.channel.send(`**\`s!revamp ${args.join(" ")}\`** is not a valid command`);
       };
     };
   };
